65dab66c28b08450e90f5e17b5329e2d7e01204e
Youngjun Woo <youngjunwoo92@gmail.com>
2023-04-15 00:03:53 -0700

    edited sanity preview

--- sanity-studio/schemas/post.js
+++ sanity-studio/schemas/post.js
@@ -52,4 +52,20 @@ export default {
       ],
     },
   ],
+  preview: {
+    select: {
+      title: 'comments.0.comment',
+      authorName: 'author.name',
+      authorUsername: 'author.username',
+      media: 'photo',
+    },
+    prepare(selection) {
+      const {title, authorName, authorUsername, media} = selection
+      return {
+        title: `${authorName} (${authorUsername})`,
+        subtitle: title,
+        media,
+      }
+    },
+  },
 }
